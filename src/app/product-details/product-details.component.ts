33cb5cb7d405456a808f80434d61be1dbc1519e1
Chillaroo <rtc145@gmail.com>
2022-02-03 17:04:14 -0500

    learned about Angular services

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,15 +12,24 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 
-  ngOnInit(){
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
     //First get product from current route
     const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute =
-    Number(routeParams.get('productId'));
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
     //Find the product that corresponds with the id provided in the route
-    this.product = products.find(product => product.id === productIdFromRoute);
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
   }
 }
